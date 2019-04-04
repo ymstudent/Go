bdde76b03e57178593cc418480546f2c9f91a749
ymstudent <m13213127921@163.com>
2019-04-04 23:35:29 +0800

    JSON

---
+++ src/programming/ch4/movie/main.go
@@ -0,0 +1,22 @@
+package main
+
+type Movie struct {
+	Title string
+	Year int `json:"released"`
+	Color bool `json:"color,omitempty"`
+	Actors []string
+}
+
+var movies = []Movie {
+	{
+		Title:"Casablanca", Year:1942, Color:false, Actors:[]string {"Humphrey Bogart", "Ingrid Bergman"},
+	},
+	{
+		Title:"Cool Hand Luke", Year:1967, Color:true, Actors:[]string {"Paul Newman"},
+	},
+	//.....
+}
+
+func main()  {
+
+}
