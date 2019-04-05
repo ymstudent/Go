1d3f514823c0d12d85eb67fd05f984374af52b2b
ymstudent <m13213127921@163.com>
2019-04-05 23:24:10 +0800

    Go数据转化为JSON格式

--- src/programming/ch4/movie/main.go
+++ src/programming/ch4/movie/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type Movie struct {
 	Title string
 	Year int `json:"released"`
@@ -17,6 +22,18 @@ var movies = []Movie {
 	//.....
 }
 
-func main()  {
+var titles []struct{Title string}
 
+func main()  {
+	//data, err := json.Marshal(movies) //将GO数据结构转换为JSON字符串
+	data, err := json.MarshalIndent(movies, "", "	") //将GO数据结构转换为格式化后的JSON字符串
+	if err != nil {
+		fmt.Printf("Json marshal failed: %s\n", err)
+	}
+	fmt.Printf("%s\n", data)
+
+	if err := json.Unmarshal(data, &titles); err != nil { //将JSON字符串解码到结构体slice titles中
+		fmt.Printf("Json unmarshal failed: %s\n", err)
+	}
+	fmt.Println(titles)
 }
