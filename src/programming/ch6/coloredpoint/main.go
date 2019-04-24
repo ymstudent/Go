412358caed69342ca50c4a5a775bf89c07ba47a8
ymstudent <m13213127921@163.com>
2019-04-24 23:59:22 +0800

    内嵌结构体选择子的优先级

--- src/programming/ch6/coloredpoint/main.go
+++ src/programming/ch6/coloredpoint/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "image/color"
+import (
+	"fmt"
+	"image/color"
+)
 
 type Point struct {
 	X, Y float64
@@ -8,6 +11,11 @@ type Point struct {
 
 type ColoredPoint struct {
 	Point
+	color color.RGBA
+}
+
+type ColoredPoint2 struct {
+	*Point
 	color color.RGBA
 }
 
@@ -22,4 +30,13 @@ func main()  {
 	p := ColoredPoint{Point{1, 2}, red}
 	p.ScaleBy(2)
 	p.Point.ScaleBy(2)
+	//通过指针类型的匿名字段，共享通用的结构使对象之间的关系更加动态，多样化
+	bule := color.RGBA{0,0,255,255}
+	a := ColoredPoint2{&Point{1,1}, bule}
+	b := ColoredPoint2{&Point{5,4}, bule}
+	a.Point = b.Point //a, b共享同一个Point
+	p.ScaleBy(2)
+	fmt.Println(a.Point, b.Point) //"{2,2} {2,2}"
+	//编译器处理内嵌结构体选择子的优先级：
+	//当前结构体选择子（eg:ColoredPoint的选择子） > 内嵌字段的选择子（eg:ColoredPoint.Point.X的选择子） > 内嵌字段的字段的选择子（eg:ColoredPoint.Point.X的选择子）
 }
