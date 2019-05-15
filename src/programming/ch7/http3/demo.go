1bcd3292da0756a83f41986739f61cd7f9d770bc
ymstudent <m13213127921@163.com>
2019-05-15 23:58:03 +0800

    增加修改处理

--- src/programming/ch7/http3/demo.go
+++ src/programming/ch7/http3/demo.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type dollars float32
@@ -16,6 +17,7 @@ func main()  {
 	mux := http.NewServeMux()
 	mux.Handle("/list", http.HandlerFunc(db.list)) //注册多个http.Handle
 	mux.Handle("/price",http.HandlerFunc(db.price))
+	mux.HandleFunc("/update", db.update)
 	log.Fatal(http.ListenAndServe("localhost:8000", mux))
 }
 
@@ -34,6 +36,22 @@ func (db database) price(w http.ResponseWriter, req *http.Request)  {
 		return
 	}
 	fmt.Fprintf(w,"%s\n", price)
+}
+
+func (db database) update(w http.ResponseWriter, req *http.Request)  {
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+	if _, ok := db[item]; !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	p, err := strconv.ParseFloat(price, 32)
+	if err != nil {
+		fmt.Fprintf(w, "invalid price")
+	}
+	db[item] = dollars(p)
+	fmt.Fprintf(w, "update sucess")
 }
 
 
