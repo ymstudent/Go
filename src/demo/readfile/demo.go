b19e2d431b24a78be56cfaf4c8b329afac50d865
ymstudent <m13213127921@163.com>
2019-06-14 22:58:09 +0800

    支付宝发券程序优化

--- src/demo/readfile/demo.go
+++ src/demo/readfile/demo.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"crypto"
-	"crypto/md5"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
+	"crypto/tls"
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/csv"
-	"encoding/hex"
 	"encoding/json"
 	"encoding/pem"
+	"flag"
 	"fmt"
 	"io/ioutil"
+	rand2 "math/rand"
 	"net/http"
 	"net/url"
 	"os"
@@ -28,44 +29,70 @@ func check(e error) {
 	}
 }
 
-
 func main() {
+	filePath := flag.String("f", "", "文件路径")
+	tplId := flag.String("t", "", "模版ID")
+	flag.Parse()
+
 	start := time.Now()
-	csvFile, err := os.Open("./dm.csv")
+	csvFile, err := os.Open(*filePath)
 	check(err)
 	defer csvFile.Close()
 
+	var wg sync.WaitGroup
 	csvReader := csv.NewReader(csvFile)
 	arr, _ := csvReader.ReadAll()
-	counter := make(chan string)
-	var wg sync.WaitGroup //工作goroutine的个数
+	counter := make(chan bool, len(arr)+1)
+	limit := make(chan bool, 1000) //用来限制goroutine数量
+	pk, err := ParsePrivateKey()
+	check(err)
+
 	for _, row := range arr {
 		wg.Add(1)
-		go func(row []string){  //通过添加显式参数，确保当go语句执行时，使用当前row值（参考5.6.1内部匿名函数中获取循环变量的问题）
+		limit <- true
+		go func(row []string) { //通过添加显式参数，确保当go语句执行时，使用当前row值（参考5.6.1内部匿名函数中获取循环变量的问题）
 			defer wg.Done()
-			params := getQuery(row)
-			counter <- params
+			success, err := sendMsg(row, *tplId, pk)
+			if err != nil {
+				fmt.Println(err)
+			}
+			counter <- success
+			<-limit
 		}(row)
 	}
+
 	go func() {
 		wg.Wait()
 		close(counter) //安全关闭通道
 	}()
-	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
-	var s []string
+
+	//统计成功与失败数量
+	successNum := 0
+	failNum := 0
 	for v := range counter {
-		s = append(s, v)
+		if v {
+			successNum++
+		} else {
+			failNum++
+		}
 	}
-	fmt.Println(len(s))
+	fmt.Printf("发券成功:%d\n", successNum)
+	fmt.Printf("发券失败:%d\n", failNum)
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
 
-func sendMsg(row []string) (success bool, err error) {
-	query := getQuery(row)
-	queryUrl := "https://openapi.alipay.com/gateway.do?"+query
-	resp, err := http.Get(queryUrl)
+func sendMsg(row []string, tplId string, pk *rsa.PrivateKey) (success bool, err error) {
+	query := getQuery(row, tplId, pk)
+	queryUrl := "https://openapi.alipay.com/gateway.do?" + query
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, //跳过https证书验证
+	}
+	c := &http.Client{
+		Transport: tr,
+	}
+	resp, err := c.Get(queryUrl)
 	if err != nil {
-		err := fmt.Errorf("请求失败:%s", err)
+		err := fmt.Errorf("请求错误:%s", err)
 		return false, err
 	}
 	defer resp.Body.Close()
@@ -87,35 +114,32 @@ func sendMsg(row []string) (success bool, err error) {
 	if res["code"] == "10000" {
 		return true, nil
 	} else {
-		err := fmt.Errorf("JSON解析失败:%s", res)
-		return false, err
+		return false, nil
 	}
 }
 
-func getQuery(row []string) string {
-	aa := make(map[string]string)
-	aa["partner_id"] = strings.Trim(row[0], "`")
-	aa["out_trade_no"] = strings.Trim(row[1], "`")
+func getQuery(row []string, tplId string, pk *rsa.PrivateKey) string {
+	recognitionInfoMap := make(map[string]string)
+	recognitionInfoMap["partner_id"] = strings.Trim(row[0], "`")
+	recognitionInfoMap["out_trade_no"] = strings.Trim(row[1], "`")
+	recognitionInfo, _ := json.Marshal(recognitionInfoMap)
 
-	bb := make(map[string]string)
-	bb["channelID"] = ""
-	bb["serialNumber"] = md5V(aa["out_trade_no"])
+	tplParamsMap := make(map[string]int)
+	tplParamsMap["channelID"] = 123456
+	r := rand2.New(rand2.NewSource(time.Now().Unix()))
+	tplParamsMap["serialNumber"] = r.Intn(100)
+	tplParams, _ := json.Marshal(tplParamsMap)
 
 	body := make(map[string]string)
-	body["tpl_id"] = ""
-	tpl_params, err := json.Marshal(bb)
-	check(err)
-	body["tpl_params"] = string(tpl_params)
+	body["tpl_id"] = tplId
 	body["recognition_type"] = "1"
-	recognition_info, err := json.Marshal(aa)
-	check(err)
-	body["recognition_info"] = string(recognition_info)
+	body["tpl_params"] = string(tplParams)
+	body["recognition_info"] = string(recognitionInfo)
 
 	data := url.Values{}
-	biz_content, err := json.Marshal(body)
-	check(err)
-	data.Set("app_id", "")
-	data.Set("biz_content", string(biz_content))
+	bizContent, _ := json.Marshal(body)
+	data.Set("app_id", "123456")
+	data.Set("biz_content", string(bizContent))
 	data.Set("charset", "utf-8")
 	data.Set("format", "JSON")
 	data.Set("method", "alipay.pass.instance.add")
@@ -123,34 +147,23 @@ func getQuery(row []string) string {
 	data.Set("timestamp", time.Now().Format("2006-01-02 15:04:05"))
 	data.Set("version", "1.0")
 	signContentBytes, _ := url.QueryUnescape(data.Encode())
-	sign := sign([]byte(signContentBytes))
+	sign := sign([]byte(signContentBytes), pk)
 	data.Set("sign", sign)
 	return data.Encode()
 }
 
-func md5V(str string) string  {
-	h := md5.New()
-	h.Write([]byte(str))
-	return hex.EncodeToString(h.Sum(nil))
-}
-
-func sign(data []byte) string {
-	prikey := ``
-
-
+func sign(data []byte, pk *rsa.PrivateKey) string {
 	h := sha256.New()
 	hType := crypto.SHA256
 	h.Write(data)
 	d := h.Sum(nil)
-	pk, err := ParsePrivateKey(prikey)
-	check(err)
 	bs, err := rsa.SignPKCS1v15(rand.Reader, pk, hType, d)
 	check(err)
 	return base64.StdEncoding.EncodeToString(bs)
 }
 
-
-func ParsePrivateKey(privateKey string) (pk *rsa.PrivateKey, err error) {
+func ParsePrivateKey() (pk *rsa.PrivateKey, err error) {
+	privateKey := ``
 	block, _ := pem.Decode([]byte(privateKey))
 	if block == nil {
 		err = fmt.Errorf("私钥格式错误1:%s", privateKey)
